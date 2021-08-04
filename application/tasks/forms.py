ae0366a06f9f26519840842afd0e77e503ab79d7
Trinity <rraj150997@gmail.com>
2021-08-05 00:26:31 +0530

    saving info sent using the form

--- application/tasks/forms.py
+++ application/tasks/forms.py
@@ -1,9 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, BooleanField
 
 
 class TaskForm(FlaskForm):
+    # name of the task
     name = StringField("Task Name")
+
+    # field that allows user to specify task done or not
+    done = BooleanField("Done")
 
     class Meta:
         # turns off recourse to cross-site request forgery attacks
--- application/tasks/views.py
+++ application/tasks/views.py
@@ -1,7 +1,7 @@
 from application import app, db
 from flask import render_template, request, redirect, url_for
 from application.tasks.models import Task
-
+from application.tasks.forms import TaskForm
 
 @app.route("/tasks", methods=["GET"])
 def tasks_index():
@@ -10,7 +10,7 @@ def tasks_index():
 
 @app.route("/tasks/new/")
 def tasks_form():
-    return render_template("new.html")
+    return render_template("new.html", form=TaskForm())
 
 
 @app.route("/tasks/<task_id>/", methods=["POST"])
@@ -23,10 +23,12 @@ def tasks_set_done(task_id):
 
 @app.route("/tasks/", methods=["POST"])
 def task_create():
-    print(request.form.get("name"))
+    form = TaskForm(request.form)
 
-    # adding row in db
-    t = Task(request.form.get("name"))
+    # get details from form
+    t = Task(form.name.data)
+    t.done = form.done.data
+
     db.session().add(t)
     db.session().commit()
 
